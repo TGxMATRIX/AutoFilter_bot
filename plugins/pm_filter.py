f66491ae31fafcc66321a2db17700b63663b7006
atrial874 <95119887+atrial874@users.noreply.github.com>
2023-01-27 21:08:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -847,7 +847,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/MatRixBotzTG')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/filesharebotusers')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help2'),
             InlineKeyboardButton('😊 About', callback_data='about')
