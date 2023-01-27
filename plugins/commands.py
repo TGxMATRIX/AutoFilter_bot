1cc5dda3afd73db153f97c205e6da4fd1006afa8
MatRixBotzTG <108185896+matrixbotz0@users.noreply.github.com>
2023-01-27 19:55:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ BATCH_FILES = {}
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[           
-            InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 📢', url='https://t.me/MatRixBotzTG')
+            InlineKeyboardButton('📢 𝚄𝙿𝙳𝙰𝚃𝙴𝚂 📢', url='t.me/filesharebotusers')
             ],[
             InlineKeyboardButton('ℹ️ 𝙷𝙴𝙻𝙿 ℹ️', url=f"https://t.me/{temp.U_NAME}?start=help")
             ]]
